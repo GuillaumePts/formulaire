1686c92b6e78ae959b74fce2feadeb654dc9c6e2
Guillaume Pitois <guillaume.pitois1@gmail.com>
2022-07-06 09:51:38 +0200

    premier commit

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+require ('./include/function.php');
+require ('./include/pdo.php');
+
+
+
+
+?>
+
+
+
+
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="">
+        <label for="nom"></label>
+        <input type="text" name="nom" id="nom">
+        <label for="prenom"></label>
+        <input type="text" name="prenom" id="prenom">
+        <label for="email"></label>
+        <input type="text" name="email" id="email">
+    </form>
+</body>
+</html>
